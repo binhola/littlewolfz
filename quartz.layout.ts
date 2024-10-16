18ff7482856b3498407763e1de3783ee531a1d19
Binh Nguyen <binhnguyen@Trams-MacBook-Air-2.local>
2024-10-16 16:06:17 +0200

    Quartz sync: Oct 16, 2024, 4:06 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,13 +6,12 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: 
-  // Component.Footer({
-  //   links: {
-  //     GitHub: "https://github.com/jackyzha0/quartz",
-  //     "Discord Community": "https://discord.gg/cRFFHYye7t",
-  //   },
-  // }),
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
+    },
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
@@ -29,6 +28,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
